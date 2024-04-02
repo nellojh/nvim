90aab9577958a0d1ff14c1bf073ff418949a800b
nelloloujh <nelloloujh@gmail.com>
2024-04-02 19:04:23 +0800

    FOR AstroNVim V4

---
+++ lua/community.lua
@@ -0,0 +1,15 @@
+--if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.utility.noice-nvim" },
+  { import = "astrocommunity.completion.cmp-cmdline" },
+  -- import/override with your plugins folder
+}
